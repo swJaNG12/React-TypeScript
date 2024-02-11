16f9a1b6d1f57605bc38aa8f266e23a7af37cea3
swJaNG12 <cbdjej38483@gmail.com>
2024-02-11 16:25:13 +0900

    라우터 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { Sidebar } from './layout';
+import { AllNotes, ArchiveNotes, ErrorPage, TagNotes, TrashNotes } from './pages';
 
 function App() {
-  return <div className="App">a</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Sidebar />
+        <div className="app__container">
+          <Routes>
+            <Route path="/" element={<AllNotes />} />
+            <Route path="/archive" element={<ArchiveNotes />} />
+            <Route path="/trash" element={<TrashNotes />} />
+            <Route path="/tag/:name" element={<TagNotes />} />
+            <Route path="/404" element={<ErrorPage />} />
+            <Route path="*" element={<Navigate to="/404" />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,5 @@ export { default as TagsModal } from './Modal/TagsModal/TagsModal';
 export { default as CreateNoteModal } from './Modal/CreateNoteModal/CreateNoteModal';
 export { default as ReadNoteModal } from './Modal/ReadNoteModal/ReadNoteModal';
 export { default as FiltersModal } from './Modal/FiltersModal/FiltersModal';
-export { default as NoteCard } from './NoteCard/NoteCard
+export { default as NoteCard } from './NoteCard/NoteCard';
 export { default as MainWrapper } from './MainWrapper/MainWrapper';
